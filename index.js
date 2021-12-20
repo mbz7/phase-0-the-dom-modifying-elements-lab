fc28d804b37b9f0df99bef9fd229d0c1b3133974
Myles <bracken.myles@gmail.com>
2021-12-20 09:24:44 -0800

    commit tree 3681f847fb6302a4d9c5cbd3b87bb0f5f0cd3988

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 // const main = document.getElementById('main');
 // main.remove();
 
+// document.querySelector("main#main").remove();
+
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory";
+// newHeader.innerHTML = "Myles is the champion!";
+
+// const element = document.createElement("div");
+
+// document.body.appendChild(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul);
+
+// const element = document.getElementById("main");
+// element.style.height = "300px";
+// element.style.backgroundColor = "#27647B";
+// element.style.textContent = "You've changed what's on the screen!";
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+// element.className = "pet-listing dog";
+// element.classList.remove("dog");
+// element.classList.add("cat", "Sale");
+
+// someElement.removeChild(someChildElement);
+
+// const ul = document.getElementsByTagName("ul")[0];
+
+// const secondChild = ul.querySelector("li:nth-child(2)");
+
+// ul.removeChild(secondChild); //remove the second li element of ul. We then pass that element as the argument to our removechild method, which removes the element from our ul.
+
+// //or
+
+// ul.remove(); //removes the whole unordered list (ul)
+
+// document.querySelector("main#main").remove();
+
+// const newHeader = document.createElement("h1");
+
+// newHeader.id = "victory";
+
+// newHeader.innerHTML = "Myles is the Champion!";
+
 document.querySelector("main#main").remove();
 
 const newHeader = document.createElement("h1");
